df0649e4fe57332f1e23bb39c26b6b5dcb718eb1
pjrobertson <robertson.patrick@gmail.com>
2010-03-23 12:02:02 +0000

    changed the 'view in 1Pwd' action to use applescript and made it more robust (hopefully fixes issue #1)

--- OnePasswordAction.m
+++ OnePasswordAction.m
@@ -39,18 +39,30 @@
 	//If there's multiple forms to fill
 	if ([[dObject stringValue] isEqualToString:@"combined objects"])
 	{
-		NSLog(@"cannot operate on multiple web forms :(");
+		NSLog(@"Cannot operate on multiple web forms :(");
+		// A nice noise to let the user know something is wrong
+		NSBeep();
 		return nil;
 			}
 	else {
-		NSLog(@"%@", [dObject objectForMeta:@"form"]);
-		CFPreferencesSetAppValue((CFStringRef)@"findUUID", [dObject objectForMeta:@"form"], CFSTR("ws.agile.1Password"));
-		CFPreferencesAppSynchronize(CFSTR("ws.agile.1Password"));
-	}
 	
-	[[NSWorkspace sharedWorkspace] launchAppWithBundleIdentifier:@"ws.agile.1Password" options:NSWorkspaceLaunchAsync additionalEventParamDescriptor:nil launchIdentifier:nil];
-	
-	return nil;
+		//NSLog(@"%@", [dObject label]);
+		//NSLog(@"%@", [dObject objectForMeta:@"locationKey"]);
+		NSString *command = @"defaults write ws.agile.1Password findUUID ";
+		command = [command stringByAppendingString:[dObject objectForMeta:@"form"]];
+		//NSLog(@"%@", command);
+		
+		// Set the args for the applescript - name for applescript 'keystroke' if 1Pwd is open and command for setting prefs if not
+		NSArray *arguments=[NSArray arrayWithObjects:[dObject label],command,nil];
+		
+		// Make dict to store errors
+		NSDictionary *dictErr=nil;
+		// Run the applesctipt
+		[[self script] executeSubroutine:@"reveal_in_1Pwd" arguments:arguments error:&dictErr];
+		// Log any errors
+		if (dictErr) NSLog(@"Create Error: %@",dictErr);	}
+
+		return nil;
 }
 
 -(void)writePlistAndFill:(QSObject *)dObject
@@ -80,6 +92,16 @@
 	[ws openURL:[NSURL URLWithString:[dObject name]]];
 	
 }
+
+// Thanks to Alcor - from the iCal plugin
+- (NSAppleScript *)script{
+	NSString *path=[[NSBundle bundleForClass:[self class]]pathForResource:@"RevealIn1Pwd" ofType:@"scpt"];
+	NSLog(@"path: %@", path);
+	NSAppleScript *script=nil;
+	if (path)
+		script=[[[NSAppleScript alloc] initWithContentsOfURL:[NSURL fileURLWithPath:path] error:nil]autorelease];	
+}
+
 
 //-(QSObject *)trashForm:(QSObject *)dObject
 //{
