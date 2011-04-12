4cfbf39379d6429c874fa36a970e061b018f5640
Patrick Robertson <robertson.patrick@gmail.com>
2011-04-12 23:50:09 +1000

    Added copyright notices (lame, I know). I want free software to always be free :)

--- OnePasswordAction.h
+++ OnePasswordAction.h
@@ -3,7 +3,22 @@
 //  OnePassword
 //
 //  Created by Patrick Robertson on 15/01/2010.
-//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//  Copyright Patrick Roberston 2010. All rights reserved.
+//
+//	This file is part of Quicksilver 1Password Module.
+//
+//	Quicksilver 1Password Module is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+//
+//	Quicksilver 1Password Module is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with Quicksilver 1Password Module.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 #import <QSCore/QSObject.h>
@@ -18,6 +33,12 @@
 #define QS1PasswordSoftwareLicense @"QS1PasswordSoftwareLicense"
 #define QS1PasswordOnlineService @"QS1PasswordOnlineService"
 #define QS1PasswordWalletItem @"QS1PasswordWalletItem"
+
+// Some things from Carbon
+#define kASAppleScriptSuite 'ascr'
+#define kASSubroutineEvent  'psbr'
+#define keyASSubroutineName 'snam'
+
 
 @interface OnePasswordAction : QSActionProvider
 {
